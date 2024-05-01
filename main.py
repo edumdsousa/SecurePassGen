4137410d0854d73c9b2f921de5e0685995c64e15
Eduardo Marques <79326679+edumdsousa@users.noreply.github.com>
2024-05-01 08:45:58 -0300

    update versao 2.0

--- main.py
+++ main.py
@@ -3,25 +3,39 @@ from tkinter import ttk
 import random
 import string
 import tkinter.messagebox as messagebox
+import os
 
-# Definir janela_gerenciar como uma variável global
-janela_gerenciar = None
+# Dicionário para armazenar os trees de cada aba
+trees = {}
 
-def gerar_senha():
+# Função para gerar senha
+def gerar_senha(comprimento_entrada, senha_entrada):
     comprimento = int(comprimento_entrada.get())
     caracteres = string.ascii_letters + string.digits + string.punctuation
     senha = ''.join(random.choice(caracteres) for _ in range(comprimento))
     senha_entrada.delete(0, tk.END)
     senha_entrada.insert(0, senha)
 
-def salvar_senha():
+# Função para salvar senha
+def salvar_senha(nome_arquivo, senha_entrada, origem_entrada):
     senha = senha_entrada.get()
     origem = origem_entrada.get()
-    with open("config.conf", "a") as arquivo:
+    with open(nome_arquivo, "a") as arquivo:
         arquivo.write(f'{origem}"{senha}\n')  # Modificado para adicionar aspas
     messagebox.showinfo("Sucesso", "Senha salva com sucesso!")
 
-def copiar_senha(event):
+# Função para editar senha
+def editar_senha(nome_arquivo):
+    # Fechar a janela de gerenciamento de senhas
+    janela_gerenciar.destroy()
+    # Abrir o arquivo onde as senhas estão salvas
+    try:
+        os.system(f"notepad {nome_arquivo}")  # Abre o arquivo no bloco de notas do Windows
+    except Exception as e:
+        messagebox.showerror("Erro", f"Erro ao abrir o arquivo: {e}")
+
+# Função para copiar senha
+def copiar_senha(event, tree):
     # Obter a linha selecionada
     item = tree.focus()
     # Obter os valores da linha selecionada
@@ -37,79 +51,96 @@ def copiar_senha(event):
         # Fechar a janela do gerenciador de senhas
         janela_gerenciar.destroy()
 
-def gerenciar_senhas_salvas():
+# Função para gerenciar senhas salvas
+def gerenciar_senhas_salvas(nome_arquivo):
     global janela_gerenciar
     # Nova janela
     janela_gerenciar = tk.Toplevel(root)
     janela_gerenciar.title("Senhas Salvas")
     janela_gerenciar.iconbitmap(bitmap="icone.ico")
 
-    # Frame para a barra de rolagem e Treeview
-    frame = ttk.Frame(janela_gerenciar)
-    frame.pack(fill='both', expand=True)
-
-    # Barra de rolagem
-    scrollbar = ttk.Scrollbar(frame, orient='vertical')
-    scrollbar.pack(side='right', fill='y')
+    # Frame principal
+    frame_principal = ttk.Frame(janela_gerenciar, padding="20")
+    frame_principal.pack(fill='both', expand=True)
 
     # Treeview para exibir senhas salvas
-    global tree
-    tree = ttk.Treeview(frame, columns=('Referência', 'Senha'), show='headings', yscrollcommand=scrollbar.set)
+    style = ttk.Style()
+    style.configure("Treeview", background="#F0F0F0", fieldbackground="#F0F0F0", foreground="black")
+    tree = ttk.Treeview(frame_principal, columns=('Referência', 'Senha'), show='headings', style="Treeview")
     tree.heading('Referência', text='Referência')
     tree.heading('Senha', text='Senha')
     tree.column('Senha', stretch=True)  # Ajuste automático do tamanho da coluna
 
-    # Configurar a barra de rolagem para rolar com o Treeview
-    scrollbar.config(command=tree.yview)
-
     # Lê senhas salvas do arquivo e adiciona ao Treeview
-    with open("config.conf", "r") as arquivo:
+    with open(nome_arquivo, "r") as arquivo:
         for linha in arquivo:
             index_aspas = linha.find('"')
             referencia = linha[:index_aspas]
             senha = linha[index_aspas+1:].strip()
             tree.insert('', 'end', values=(referencia, senha))
 
-    tree.pack(fill='both', expand=True)
+    tree.pack(fill='x', expand=True)
+
+    # Esticar o botão para ocupar todo o frame horizontalmente
+    editar_botao = ttk.Button(frame_principal, text="Editar Senha", style="TButton", command=lambda: editar_senha(nome_arquivo))
+    editar_botao.pack(fill='x', pady=10)
 
     # Adicionar evento de duplo clique para copiar a senha
-    tree.bind('<Double-1>', copiar_senha)
+    tree.bind('<Double-1>', lambda event: copiar_senha(event, tree))
 
+    # Armazenar o tree no dicionário
+    trees[nome_arquivo] = tree
+
+# Função para criar uma nova aba
+def criar_aba(nome_visual, nome_arquivo):
+    aba = ttk.Frame(notebook, padding="20")
+    notebook.add(aba, text=nome_visual)
+
+    # Widgets
+    comprimento_label = ttk.Label(aba, text="Quantidade de Dígitos:", style="TLabel")
+    comprimento_entrada = ttk.Entry(aba)
+    gerar_botao = ttk.Button(aba, text="Gerar Senha", style="TButton", command=lambda: gerar_senha(comprimento_entrada, senha_entrada))
+    senha_entrada = ttk.Entry(aba)
+    origem_label = ttk.Label(aba, text="Referência:", style="TLabel")
+    origem_entrada = ttk.Entry(aba, width=50)
+    salvar_botao = ttk.Button(aba, text="Salvar", style="TButton", command=lambda: salvar_senha(nome_arquivo, senha_entrada, origem_entrada))
+    gerenciar_botao = ttk.Button(aba, text="Gerenciar Senhas Salvas", style="TButton", command=lambda: gerenciar_senhas_salvas(nome_arquivo))
+
+    # Layout
+    comprimento_label.grid(row=0, column=0, padx=10, pady=10, sticky="w")
+    comprimento_entrada.grid(row=0, column=1, padx=10, pady=10, sticky="ew")
+    gerar_botao.grid(row=1, column=0, columnspan=2, padx=10, pady=10, sticky="ew")
+    senha_entrada.grid(row=2, column=0, columnspan=2, padx=10, pady=10, sticky="ew")
+    origem_label.grid(row=3, column=0, padx=10, pady=10, sticky="w")
+    origem_entrada.grid(row=3, column=1, padx=10, pady=10, sticky="ew")
+    salvar_botao.grid(row=4, column=0, columnspan=2, padx=10, pady=10, sticky="ew")
+    gerenciar_botao.grid(row=5, column=0, columnspan=2, padx=10, pady=10, sticky="ew")
+
+    # Configuração do estilo
+    style = ttk.Style()
+    style.configure('TButton', foreground='white', background='#007BFF', font=('Arial', 10))
+    style.map('TButton', background=[('active', '#0056B3')])
+    style.configure('TLabel', foreground='#007BFF', font=('Arial', 10))
+
+# Configuração da janela principal
 root = tk.Tk()
 root.title("Gerador de Senha Segura")
-root.configure(bg="#f0f0f0")  # Cor de fundo
-root.iconbitmap(bitmap="icone.ico")
 
-# Impede o redimensionamento da janela
-root.resizable(False, False)
-
-# Personalização de cores
-cor_botao = "#4caf50"
-cor_botao_salvar = "#2196f3"
-cor_texto = "#333333"
+# Carregar um estilo mais moderno para os widgets
+style = ttk.Style()
+style.theme_use('clam')
 
 # Widgets
-comprimento_label = tk.Label(root, text="Quantidade de Dígitos:", bg="#f0f0f0", fg=cor_texto)
-comprimento_entrada = tk.Entry(root)
-gerar_botao = tk.Button(root, text="Gerar Senha", command=gerar_senha, bg=cor_botao, fg="white")
-senha_entrada = tk.Entry(root)
-origem_label = tk.Label(root, text="Referência:", bg="#f0f0f0", fg=cor_texto)
-origem_entrada = tk.Entry(root, width=50)  # Aumentando o tamanho do campo de entrada
-salvar_botao = tk.Button(root, text="Salvar", command=salvar_senha, bg=cor_botao_salvar, fg="white")
-gerenciar_botao = tk.Button(root, text="Gerenciar Senhas Salvas", command=gerenciar_senhas_salvas, bg="#ff9800", fg="white")
+notebook = ttk.Notebook(root)
+notebook.pack(fill='both', expand=True)
 
-# Layout
-comprimento_label.grid(row=0, column=0, padx=10, pady=10, sticky="w")
-comprimento_entrada.grid(row=0, column=1, padx=10, pady=10, sticky="ew")
-gerar_botao.grid(row=1, column=0, columnspan=2, padx=10, pady=10, sticky="ew")
-senha_entrada.grid(row=2, column=0, columnspan=2, padx=10, pady=10, sticky="ew")
-origem_label.grid(row=3, column=0, padx=10, pady=10, sticky="w")
-origem_entrada.grid(row=3, column=1, padx=10, pady=10, sticky="ew")
-salvar_botao.grid(row=4, column=0, columnspan=2, padx=10, pady=10, sticky="ew")
-gerenciar_botao.grid(row=5, column=0, columnspan=2, padx=10, pady=10, sticky="ew")
+# Criação das abas
+abas = [("Layer1", "Layer1.conf"), ("Layer2", "Layer2.conf"), ("Layer3", "Layer3.conf"), ("Layer4", "Layer4.conf"), ("Layer5", "Layer5.conf")]
+for nome_visual, nome_arquivo in abas:
+    criar_aba(nome_visual, nome_arquivo)
 
-# Centralizar widgets verticalmente
-for i in range(6):
-    root.grid_rowconfigure(i, weight=1)
+# Configuração do fundo
+root.configure(bg="#F0F0F0")
 
+# Laço principal
 root.mainloop()
